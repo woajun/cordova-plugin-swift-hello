74a5b2284cf6ef128fb3ba36b791e4fc50db7b15
juni <woajun@naver.com>
2024-02-29 15:02:31 +0900

    sayHello 함수 추가

--- src/ios/sayHelloPlugin.m
+++ src/ios/sayHelloPlugin.m
@@ -2,27 +2,33 @@
 
 #import <Cordova/CDV.h>
 
-@interface sayHelloPlugin : CDVPlugin {
-  // Member variables go here.
-}
+@interface sayHelloPlugin : CDVPlugin
 
 - (void)coolMethod:(CDVInvokedUrlCommand*)command;
+- (void)sayHello:(CDVInvokedUrlCommand*)command;
+
 @end
 
 @implementation sayHelloPlugin
 
-- (void)coolMethod:(CDVInvokedUrlCommand*)command
-{
+- (void)coolMethod:(CDVInvokedUrlCommand*)command {
     CDVPluginResult* pluginResult = nil;
     NSString* echo = [command.arguments objectAtIndex:0];
-
+    
     if (echo != nil && [echo length] > 0) {
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
     } else {
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
     }
-
+    
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+- (void)sayHello:(CDVInvokedUrlCommand*)command {
+    UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"Hello" message:nil preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction* defaultAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil];
+    [alert addAction:defaultAction];
+    [self.viewController presentViewController:alert animated:YES completion:nil];
 }
 
 @end
--- www/sayHelloPlugin.js
+++ www/sayHelloPlugin.js
@@ -3,3 +3,7 @@ var exec = require('cordova/exec');
 exports.coolMethod = function (arg0, success, error) {
     exec(success, error, 'sayHelloPlugin', 'coolMethod', [arg0]);
 };
+
+exports.sayHello = function (arg0, success, error) {
+    exec(success, error, 'sayHelloPlugin', 'sayHello', [arg0]);
+};
