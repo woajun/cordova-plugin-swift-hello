40ea5e544c8554c896d0fc15d6d3e366037b0cfc
juni <woajun@naver.com>
2024-03-05 15:40:30 +0900

    카카오 변수 사용

--- src/ios/sayHelloPlugin.swift
+++ src/ios/sayHelloPlugin.swift
@@ -8,36 +8,46 @@ class sayHelloPlugin: CDVPlugin {
 
   @objc(sayHello:)
   func sayHello(_ command: CDVInvokedUrlCommand) {
-    print("Hello, world!")
-
-    if let message = command.arguments.first as? String {
-      let alert = UIAlertController(title: "Hello Swift", message: message, preferredStyle: .alert)
-      let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
-      alert.addAction(defaultAction)
-      if let viewController = UIApplication.shared.keyWindow?.rootViewController {
-        viewController.present(alert, animated: true, completion: nil)
-      }
-      let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK)
-      self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
+    guard let message = command.arguments.first as? String else {
+      return
     }
+
+    let alert = UIAlertController(title: "Hello Swift", message: message, preferredStyle: .alert)
+    let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+    alert.addAction(defaultAction)
+    if let viewController = UIApplication.shared.keyWindow?.rootViewController {
+      viewController.present(alert, animated: true, completion: nil)
+    }
+    let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK)
+    self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
   }
 
   @objc(initKakaoSDK:)
   func initKakaoSDK(_ command: CDVInvokedUrlCommand) {
-    KakaoSDK.initSDK(appKey: "ba758ad99735aef083caea6309ee58b4")
+    guard let appKey = command.arguments.first as? String else {
+      return
+    }
+
+    KakaoSDK.initSDK(appKey: appKey)
+
     let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK)
     self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
   }
 
   @objc(addChannel:)
   func addChannel(_ command: CDVInvokedUrlCommand) {
-    TalkApi.shared.addChannel(channelPublicId: "_hlZXG") { error in
+    guard let channelPublicId = command.arguments.first as? String else {
+      return
+    }
+
+    TalkApi.shared.addChannel(channelPublicId: channelPublicId) { error in
       if let error = error {
         print(error)
       } else {
         print("addChannel() success.")
       }
     }
+
     let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK)
     self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
   }
